0142e91638a9b671ba33f8a541f1b2abbb1c1267
dylangrandmont <dylan.grandmont@seequent.com>
2019-05-12 16:23:30 -0600

    introduce basic form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import SignatureCanvas from 'react-signature-canvas'
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
 
-function App() {
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  canvas: {
+    border: '1px solid black',
+  },
+  title: {
+    color: 'white',
+  }
+};
+
+function App(props) {
+  const { classes } = props;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Pre-Shift Pro
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <TextField
+        id="standard-name"
+        label="Company"
+        margin="normal"
+      />
+
+      <TextField
+        id="standard-name"
+        label="Date"
+        margin="normal"
+      />
+      <TextField
+        id="standard-name"
+        label="Location"
+        margin="normal"
+      />
+      <TextField
+        id="standard-name"
+        label="Shift"
+        margin="normal"
+      />
+      <TextField
+        id="standard-name"
+        label="Equipment #"
+        margin="normal"
+      />
+      <TextField
+        id="standard-name"
+        label="Type"
+        margin="normal"
+      />
+
+      <TextField
+        id="standard-name"
+        label="Explanation of Defects"
+        margin="normal"
+      />
+
+      <TextField
+        id="standard-name"
+        label="Operator Name"
+        margin="normal"
+      />
+      <SignatureCanvas
+        penColor='green'
+        canvasProps={{width: 400, height: 80, className: "canvas"}}
+    />,
+
+      <TextField
+        id="standard-name"
+        label="Mechanics Signature"
+        margin="normal"
+      />
+      <SignatureCanvas
+        penColor='green'
+        canvasProps={{width: 400, height: 80, className: "canvas" }}
+      />,
+
+      <Button>
+        Submit
+      </Button>
     </div>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
